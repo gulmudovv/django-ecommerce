82560cd67e39e16bd784e74d59c64e36afba92af
gulmudovv <khavadihoney@gmail.com>
2021-01-13 13:06:02 +0300

    initial commit

--- djangoShop/settings.py
+++ djangoShop/settings.py
@@ -134,8 +134,8 @@ MEDIA_URL = '/images/'
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "khavadihoney@gmail.com"
-EMAIL_HOST_PASSWORD = "medestvsegda"
+EMAIL_HOST_USER = "********"
+EMAIL_HOST_PASSWORD = "*********"
 EMAIL_USE_TLS = True
 
 SERVER_EMAIL = EMAIL_HOST_USER
